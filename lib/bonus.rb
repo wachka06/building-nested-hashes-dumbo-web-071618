a06619649bed34bccefad1298e534b5c5057e7a3
wachka06 <wachka06@gmail.com>
2018-07-10 15:17:09 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,14 @@ def bonus
   }
 
   #code your solution here:
-
+  epic_tragedy = {
+    montague: {
+      
+    },
+    capulet: {
+      
+    }
+  }
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
